f939b2233063239122fe9e5d52c2f16e6691e32c
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-26 21:21:18 +0530

    Added:Program to Test Annotations

--- AnnotationExample.java
+++ AnnotationExample.java
@@ -1,7 +1,44 @@
 package com.JavaAnnotations;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 public class AnnotationExample {
+    @Override
+    @MethodInfo(author = "Nishu", comments = "Main Method", date = "Aug 20 2021", revision = 1)
+    public String toString() {
+        return "Overridden toString method";
+    }
+
+    @Deprecated
+    @MethodInfo(comments = "Deprecated Method", date = "August 20 2021")
+    public static void oldMethod() {
+        System.out.println("Old method , don't use it!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Java Annotation Problems");
+        try {
+            for (Method method : AnnotationExample.class.getMethods()) {
+                // checks if methodInfo annotation present for method or not
+                if (method.isAnnotationPresent(MethodInfo.class)) {
+                    try {
+                        // iterates all annotations available in method
+                        for (Annotation anno : method.getDeclaredAnnotations()) {
+                            System.out.println("Annotation in Method " + method + " :" + anno);
+
+                        }
+                        MethodInfo methodAnno = method.getDeclaredAnnotation(MethodInfo.class);
+                        if (methodAnno.revision() == 1) {
+                            System.out.println("Method with Revision no 1 = " + method);
+
+                        }
+                    } catch (Throwable ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
     }
 }
