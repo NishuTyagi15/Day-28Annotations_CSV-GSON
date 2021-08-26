34a16edcb1439f3181e2f79a97423b1553230a22
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-26 21:12:00 +0530

    Added:Welcome Message

---
+++ AnnotationExample.java
@@ -0,0 +1,7 @@
+package com.JavaAnnotations;
+
+public class AnnotationExample {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java Annotation Problems");
+    }
+}
