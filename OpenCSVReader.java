373655bc1be5a63efd436fe64bea49823087e70a
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-26 22:14:44 +0530

    Refactored:Program to Read CSV File into List<String[]>

--- OpenCSVReader.java
+++ OpenCSVReader.java
@@ -1,12 +1,14 @@
 package com.JavaAnnotations;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCSVReader {
     private static final String SampleCSVFile = "src\\test\\java\\com\\JavaAnnotations\\csvfile.csv";
@@ -17,14 +19,19 @@ public class OpenCSVReader {
             Reader reader = Files.newBufferedReader(Paths.get(SampleCSVFile));
             csvReader = new CSVReader(reader);
         } finally {
-            //Reading records one by one in string array
-            String[] nextRecord;
-            while ((nextRecord = csvReader.readNext()) != null) {
-                    System.out.println("Name:" + nextRecord[0]);
-                    System.out.println("Email:" + nextRecord[1]);
-                    System.out.println("Phone:" + nextRecord[2]);
-                    System.out.println("Country:" + nextRecord[3]);
-                    System.out.println("==============================");
+            //Reading all records at once into a List<String[]>
+            List<String[]> records = null;
+            try {
+                records = csvReader.readAll();
+            } catch (CsvException e) {
+                e.printStackTrace();
+            }
+            for (String[] record : records) {
+                System.out.println("Name:" + record[0]);
+                System.out.println("Email:" + record[1]);
+                System.out.println("Phone:" + record[2]);
+                System.out.println("Country:" + record[3]);
+                System.out.println("==============================");
             }
         }
     }
